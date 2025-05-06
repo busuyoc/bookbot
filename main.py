bbf4651711962a77751f2dfde55b5387d8a93618
busuyoc <busuioc.p.claudiu@gmail.com>
2025-05-06 13:53:54 +0300

    Implement word count

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from stats import count_words
+
 def get_book_text(fp):
     with open(fp) as f:
         file_contents = f.read()
-        print(file_contents)
+        count_words(file_contents)
         
 def main():
     get_book_text("./books/frankenstein.txt")
+    
 
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+def count_words(text):
+    words_list= text.split()
+    num_words = len(words_list)
+    print(f"{num_words} words found in the document")
+
